3ff5e0f0d41eab564515aa95581ebed38c083b19
Swati <swatigarule5@gmail.com>
2024-03-31 16:42:51 +0530

    String is displayed

---
+++ Spring/src/com/dxc/Strings.java
@@ -0,0 +1,10 @@
+package com.dxc;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Strings are immutable in java");
+	}
+
+}
